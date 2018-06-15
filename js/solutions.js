11b298dd5f85874a74c902cb78b78e0931448202
David Epps <kaliforniaco@gmail.com>
2018-06-14 21:32:27 -0600

    Commit 1 - Verbal questions -s

--- js/solutions.js
+++ js/solutions.js
@@ -1,3 +1,9 @@
+//- 2. The difference between a parameter and an argument is that a PARAPATER is defined when
+//a variable or function is declared, while an ARGUMENT is passed as a value when calling
+//a function.
+
+//- 3. Within a function, while CONSOLE.LOG simply writes arguments to the Console,
+//RETURN is used to store that value for later use.
 
 
 console.log("END OF LINE")
