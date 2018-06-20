eb7c2147b42f87b82d13a17372b3b5d0c0261659
David Epps <kaliforniaco@gmail.com>
2018-06-20 12:30:21 -0600

    Commit 5 - Sum Array

--- js/solutions.js
+++ js/solutions.js
@@ -26,7 +26,7 @@ if (checkPalindrome("EvadeMeDave")) {
 
 //- Tested case and boolean: good
 
-
+// Number Sum
 
 const sumNumbers=(value) => {
     sum = 0;
@@ -49,6 +49,42 @@ const calculateSide = (sideA, sideB) => {  //**confirm varnamecase!
 }
 console.log(`${calculateSide(8, 6)}`);
 
+//-
 
+//Sum Array
+const sumArray=(numbersArr) => {
+let sum=0;
+for (let i=0; i<numbersArr.length; i++){
+    sum += (numbersArr[i]);  
+}
+return sum;
+};
+
+console.log(sumArray([1,2,3,4,5,6]));
+
+
+// Prime Numbers
+
+function printPrime(value) {
+    var primes = [];
+    for(var i = 2; i < value; i++) {
+        primes[i] = true;
+    }
+    var limit = Math.sqrt(value);
+    for(var i = 2; i < limit; i++) {
+        if(primes[i] === true) {
+            for(var j = i * i; j < value; j += i) {
+                primes[j] = false;
+            }
+        }
+    }
+    for(var i = 2; i < value; i++) {
+        if(primes[i] === true) {
+            console.log(i + " " + primes[i]);
+        }
+    }
+}
+
+printPrime(100);
 
 
